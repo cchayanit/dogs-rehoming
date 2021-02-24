6d0943986e488eb1bfa9863fc253c886312a337d
cchayanit <chayanit.pm@gmail.com>
2021-02-24 20:38:44 +0700

    routerlink each component

--- Front-end/src/app/app-routing.module.ts
+++ Front-end/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
+import { AboutComponent } from './about/about.component';
+import { FoundationComponent } from './foundation/foundation.component';
+import { GuideComponent } from './guide/guide.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'guide', component: GuideComponent},
+  {path: 'foundation', component: FoundationComponent},
+  {path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Front-end/src/app/app.module.ts
+++ Front-end/src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { GuideComponent } from './guide/guide.component';
+import { FoundationComponent } from './foundation/foundation.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    GuideComponent,
+    FoundationComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,

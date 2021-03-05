c81038529fa91ff4a1e2dc6e8a6ebcd6c3011e50
cchayanit <chayanit.pm@gmail.com>
2021-03-05 21:48:17 +0700

    use the title service

--- Front-end/src/app/guide/guide.component.ts
+++ Front-end/src/app/guide/guide.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-guide',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GuideComponent implements OnInit {
 
-  constructor() { }
+  constructor(private title: Title) { }
 
   ngOnInit(): void {
+    this.title.setTitle('คู่มือการเลี้ยงสุนัข');
   }
 
 }
